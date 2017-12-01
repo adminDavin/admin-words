e8652a68759a6929cd401289744202346bdf96ae
davin <davinzhang@outlook.com>
2017-12-01 12:58:49 +0800

    pdf

---
+++ src/admin-words/script/bean/WordsInfo.js
@@ -0,0 +1,40 @@
+ 
+
+class WordsInfo {
+    constructor(id,wordsName,documentId,count,findPage,findTime,remark) {
+        this._id= id ;
+        this._wordsName= wordsName;
+        this._documentId=  documentId;
+        this._count= count;
+        this._findPage= findPage;
+        this._findTime=   findTime;
+        this._remark= remark;
+    } 
+    getId(){
+        return this._id;
+    }
+    getDesc(){
+        return {
+            id :"序号", 
+            wordsName: "检索词" ,
+            documentId:"文档ID", 
+            count:"总计次数", 
+            findPage :"录入页码", 
+            findTime :"录入时间",  
+            remark :"备注" 
+        }
+    }
+    getJson(){
+        return {
+            id :this._id, 
+            wordsName: this._wordsName ,
+            documentId:this._documentId, 
+            count:  this._count, 
+            findPage :this._findPage, 
+            findTime :this._findTime,  
+            remark :this._remark 
+        }
+    }
+}
+
+export default WordsInfo;
--- src/admin-words/script/viewer.js
+++ src/admin-words/script/viewer.js
@@ -1,6 +1,29 @@
-import "pdfjs-dist/build/pdf.js";
-import "pdfjs-dist/web/pdf_viewer.js";
-import "pdfjs-dist/web/pdf_viewer.css";
-console.log("this viewer");
+import WordsInfo from './bean/WordsInfo' 
 
-console.log(viewer);
+let words=new WordsInfo(); 
+
+console.log(words.getJson()); 
+const tplTable = (item)=>{
+    var s = templateData[0];
+    for (var i = 1; i < arguments.length; i++) {
+        var arg = String(arguments[i]);
+        // Escape special characters in the substitution.
+        s += arg.replace(/&/g, "&amp;")
+                .replace(/</g, "&lt;")
+                .replace(/>/g, "&gt;");
+        // Don't escape special characters in the template.
+        s += templateData[i];
+      }
+      return s;
+    return 
+}
+
+class MainInfo{
+    init(){
+        console.log('this is init string;');
+        $('#words-table').append("<div>ddddd</div>");
+    }
+}
+
+let mainInfo=new MainInfo();
+mainInfo.init();
