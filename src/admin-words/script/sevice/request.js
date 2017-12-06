15b32c218c6281c394981bdbe0a0b684e1cc3a1d
davin <davinzhang@outlook.com>
2017-12-06 12:07:59 +0800

    build

--- src/admin-words/script/sevice/request.js
+++ src/admin-words/script/sevice/request.js
@@ -1,31 +1,27 @@
- 
- 
 import axios from "axios";
 
-const request={
-    getModel: function(){
-        console.log("fdfd");
-    }, 
-    apiLoadFile:function(resp,data){
-        axios({
-            method: "post",
-            url: "/admin/loadDocument",
-            data: data
-          })
-            .then(function(response) {
-              if (response.status == 200) {
-                if (response.data.code == 200) {
-                  let result = response.data.result;
-                  resp(result);
-                }
-              }
-            })
-            .catch(function(error) {
-              console.log(error);
-            });
-    }
-    
+const request = {
+  getModel: function() {
+    console.log("fdfd");
+  },
+  apiLoadFile: function(resp, data) {
+    axios({
+      method: "post",
+      url: "/admin/loadDocument",
+      data: data
+    })
+      .then(function(response) {
+        if (response.status == 200) {
+          if (response.data.code == 200) {
+            let result = response.data.result;
+            resp(result);
+          }
+        }
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  }
 };
 
-export default  request;
-
+export default request;
