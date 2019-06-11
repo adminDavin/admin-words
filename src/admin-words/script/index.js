cca55444ab5f3c35955d55c800b9c77faf4f126c
davin <davinzhang@outlook.com>
2019-06-12 07:53:42 +0800

    bug fix for upgrade webpack

--- src/admin-words/script/index.js
+++ src/admin-words/script/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import getRouter from "./routers/index";
+const ReactDOM = require("react-dom");
 
 ReactDOM.render(getRouter(), document.getElementById("content"));
--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -3,5 +3,5 @@ const merge = require("webpack-merge");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 module.exports = merge(devConfig, {
   mode: "production",
-  devtool: 'inline-source-map',
+  devtool: 'false',
 });
