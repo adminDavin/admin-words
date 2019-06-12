05acbbd2def97af59fd77b8d9c94ac36a9c804ec
davinzhang@outlook.com <33772309+adminDavin@users.noreply.github.com>
2019-06-12 17:54:42 +0800

    Update index.js

--- src/admin-words/script/index.js
+++ src/admin-words/script/index.js
@@ -1,5 +1,8 @@
-import React from "react";
-import getRouter from "./routers/index";
-const ReactDOM = require("react-dom");
+import ReactDOM from 'react-dom';
+import getRouter from './routers/index';
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "bootstrap-css-only/css/bootstrap.min.css";
+import "mdbreact/dist/scss/mdb.scss";
 
-ReactDOM.render(getRouter(), document.getElementById("content"));
+
+ReactDOM.render(getRouter(), document.getElementById('content'));
