2f6ee6254589f30088c0039c89a2f833442851eb
davinzhang@outlook.com <33772309+adminDavin@users.noreply.github.com>
2019-06-13 18:18:55 +0800

    Update index.js

--- src/admin-words/script/index.js
+++ src/admin-words/script/index.js
@@ -4,7 +4,8 @@ import Application from './routers/index';
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "bootstrap-css-only/css/bootstrap.min.css";
 import "./../style/admin-words.scss";
-import { BrowserRouter as Router } from 'react-router-dom';
 
 
-ReactDOM.render(<Router><Application /></Router>, document.getElementById('root'));
+
+ReactDOM.render(<Application />, document.getElementById('root'));
+
